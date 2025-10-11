e35d8e69489724e0503e7e8d7958b5ec930dfc9f
Damo888777 <60minuteapps@MacBookPro.Home>
2025-10-11 15:58:07 +0100

    updated packages again..

--- metro.config.js
+++ metro.config.js
@@ -3,4 +3,8 @@ const { withNativeWind } = require('nativewind/metro');
 
 const config = getDefaultConfig(__dirname);
 
+// Füge .fx als gültige Source-Extension hinzu
+config.resolver.sourceExts.push('fx');
+
+// Wende NativeWind Config an
 module.exports = withNativeWind(config, { input: './global.css' });
